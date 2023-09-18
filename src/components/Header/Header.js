12151eed6b0202a6a5306732ade9d2092ef3bfdd
AndresBarriga <andresbarrigaru@gmail.com>
2023-09-18 17:30:05 +0200

    About page/ Project Page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { FiMenu, FiMoon, FiSun, FiX } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import useThemeSwitcher from '../../hooks/useThemeSwitcher';
-//import HireMeModal from '../HireMeModal';
 //import logoLight from '../../images/logo-light.svg';
 //import logoDark from '../../images/logo-dark.svg';
 import { motion } from 'framer-motion';
@@ -18,20 +17,6 @@ const AppHeader = () => {
 			setShowMenu(true);
 		} else {
 			setShowMenu(false);
-		}
-	}
-
-	function showHireMeModal() {
-		if (!showModal) {
-			document
-				.getElementsByTagName('html')[0]
-				.classList.add('overflow-y-hidden');
-			setShowModal(true);
-		} else {
-			document
-				.getElementsByTagName('html')[0]
-				.classList.remove('overflow-y-hidden');
-			setShowModal(false);
 		}
 	}
 
@@ -130,13 +115,11 @@ const AppHeader = () => {
 						Contact
 					</Link>
 					<div className="border-t-2 pt-3 sm:pt-0 sm:border-t-0 border-primary-light dark:border-secondary-dark">
-						<span
-							onClick={showHireMeModal}
-							className="font-general-medium sm:hidden block text-left text-md bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2 duration-300 w-24"
-							aria-label="Hire Me Button"
-						>
-							<Button title="Hire Me" />
-						</span>
+						<Link to="/contact">
+  <span className="font-general-medium sm:hidden block text-left text-md bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2 duration-300 w-24" aria-label="Book a Call">
+    <Button title="Book a Call" />
+  </span>
+</Link>
 					</div>
 				</div>
 
@@ -170,13 +153,14 @@ const AppHeader = () => {
 				
         <div className="hidden sm:flex justify-between items-center flex-col md:flex-row">
 					<div className="hidden md:flex">
-						<span
-							onClick={showHireMeModal}
-							className="text-md font-general-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-md px-5 py-2.5 duration-300"
-							aria-label="Hire Me Button"
-						>
-							<Button title="Hire Me" />
-						</span>
+						<Link to="/contact">
+      <span
+        className="text-md font-general-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-md px-5 py-2.5 duration-300"
+        aria-label="Hire Me Button"
+      >
+        <Button title="Let's talk" />
+      </span>
+    </Link>
 
 					</div> 
           </div> 
@@ -197,16 +181,6 @@ const AppHeader = () => {
 				{/*</div>*/}
 			 </motion.nav>
       
-			// {/* Hire me modal
-			// <div>
-			// 	{showModal ? (
-			// 		<HireMeModal
-			// 			onClose={showHireMeModal}
-			// 			onRequest={showHireMeModal}
-			// 		/>
-			// 	) : null}
-			// 	{showModal ? showHireMeModal : null}
-			// </div> */}
 		
 	);
 };
