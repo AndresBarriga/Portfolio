e033229875e1b29e38fab7208b574d9a01b8ba82
AndresBarriga <135598192+AndresBarriga@users.noreply.github.com>
2023-09-29 10:04:43 +0200

    Update MySkills.jsx
    Change in div flex width for from medium screen size md:w-4/5

--- src/components/shared/MySkills.jsx
+++ src/components/shared/MySkills.jsx
@@ -24,7 +24,7 @@ const MySkills = () => {
     
               <div className="flex flex-col md:justify-between items-center md:flex-row mt-12 md:mt-2">
                       
-                      <div className="flex flex-col justify-center md:w-2/4 ">
+                      <div className="flex flex-col justify-center md:w-4/5 ">
                           <h1 className="text-2xl font-general-bold sm:text-3xl md:text-4xl">Building <span className='text-indigo-500'>products for People</span></h1>
                           <p className=" text-gray-500 text-base mt-4 font-general-light  md:text-lg lg:text-1xl xl:text-2xl">With my startup background, I place <span className='text-indigo-500'>customer experience</span> at the forefront of product development. This customer-centric approach drives my passion for crafting products that <span className='text-indigo-500'>truly understand and enhance </span>user experiences. </p>
                       </div>
