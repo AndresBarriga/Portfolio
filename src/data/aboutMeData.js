1f7d473d71b4d13d9f297bbda5c0d68e07fe06b7
AndresBarriga <135598192+AndresBarriga@users.noreply.github.com>
2023-09-29 10:15:03 +0200

    Update aboutMeData.js

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,18 +1,18 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: "I'm driven to create products that make a difference, so that I became passionate about customer-centric design and using data to make informed decisions and reduce waste. ",
-	},
-	{
-		id: 3,
-		bio: "I believe that by using this approach we can build products that truly make a difference and positve impact on the world. For example, in my current role I lead the development of a Telemedicine App that is helping thousend of patients to monitor their vital data signs from the comfort of their homes. ",
-	},
-	{
-		id: 4,
-		bio: "I am always looking for new challenges and opportunities to learn and grow. I am also a team player and I thrive on collaborating with others to build successful products.",
-	},
-	{
-		id: 5,
-		bio: "Join me as we explore the fascinating intersection of technology, innovation, and product management.", 
-	},
+  {
+    id: 1,
+    bio: "I'm driven to create products that make a difference, so I became passionate about customer-centric design and using data to make informed decisions and reduce waste.",
+  },
+  {
+    id: 3,
+    bio: "I believe that by using this approach, we can build products that truly make a difference and have a positive impact on the world. For example, in my current role, I led the development of a Telemedicine App that is helping thousands of patients monitor their vital signs from the comfort of their homes.",
+  },
+  {
+    id: 4,
+    bio: "I am always looking for new challenges and opportunities to learn and grow. I am also a team player and thrive on collaborating with others to build successful products.",
+  },
+  {
+    id: 5,
+    bio: "Join me as we explore the fascinating intersection of technology, innovation, and product management.",
+  }
 ];
